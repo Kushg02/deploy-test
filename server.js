a170307130a1fc2d23375404ec527aae4ccd5480
Kush Garg <kushg02@gmail.com>
2020-06-09 18:27:19 -0700

    yesyes
    yes

--- server.js
+++ server.js
@@ -47,13 +47,13 @@ require("./app/routes/note.routes.js")(app);
 
 // ........
 
+// listen for requests
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-// listen for requests
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
 });
